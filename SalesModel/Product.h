43cea6165a49bdea7722174e364e16fc414d8a7b
R0NS1T0 <a20202491@pucp.edu.pe>
2022-10-05 21:29:53 -0500

    Agregar archivos de proyecto.

---
+++ SalesModel/Product.h
@@ -0,0 +1,20 @@
+/**
+ * Project Untitled
+ */
+#pragma once
+
+using namespace System;
+
+namespace SalesModel {
+    public ref class Product {
+    public:
+        property int Code;
+        property double Stock;
+        property String^ Description;
+        property double Price;
+        property String^ Size;
+        property String^ Color;
+        property char Status;
+        property array<Byte>^ Photo;
+    };
+}
